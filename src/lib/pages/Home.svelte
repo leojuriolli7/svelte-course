4bd38c36f86e4a26c4aae44329cf0d2aa83ba801
leojuriolli7 <leojuriolli@gmail.com>
2023-08-31 20:09:09 -0300

    feat: custom interpolate

--- src/lib/pages/Home.svelte
+++ src/lib/pages/Home.svelte
@@ -1,25 +1,47 @@
 <script>
   import { cubicInOut } from "svelte/easing";
   import { tweened } from "svelte/motion";
+  import { interpolateRgb } from "d3-interpolate";
 
-  const size = tweened(
-    { width: 100, height: 100 },
-    { duration: 200, easing: cubicInOut }
+  const boxProps = tweened(
+    { width: 100, height: 100, color: "purple" },
+    {
+      duration: 200,
+      easing: cubicInOut,
+      interpolate: (old, final) => {
+        return (time) => {
+          const deltaWidth = final.width - old.width;
+          const deltaHeight = final.height - old.height;
+          const color = interpolateRgb(old.color, final.color)(time);
+
+          return {
+            width: old.width + deltaWidth * time,
+            height: old.height + deltaHeight * time,
+            color,
+          };
+        };
+      },
+    }
   );
 </script>
 
 <button
   on:click={async () => {
     // if animation is interrupted, this never resolves.
-    await size.set({
-      width: Math.random() * 500,
-      height: Math.random() * 500,
-    });
+    await boxProps.set(
+      {
+        width: Math.random() * 500,
+        height: Math.random() * 500,
+        color: `#${Math.floor(Math.random() * 16777215).toString(16)}`,
+      }
+      // can override config inside the set:
+      // { duration: 4000 }
+    );
 
     console.log("set is done");
-  }}>Randomize size</button
+  }}>Randomize</button
 >
 
 <div
-  style="background-color: purple; width: {$size.width}px; height: {$size.height}px"
+  style="background-color: {$boxProps.color}; width: {$boxProps.width}px; height: {$boxProps.height}px"
 />
